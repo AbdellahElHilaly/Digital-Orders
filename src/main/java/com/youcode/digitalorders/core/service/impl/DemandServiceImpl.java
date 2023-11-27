04eee96fc0de567c3e3aec879bb27a8287fc3c36
abdellah el hilaly <abdellah.elhilaly.96@gmail.com>
2023-11-27 13:31:33 +0100

    empty commit

--- src/main/java/com/youcode/digitalorders/core/service/impl/DemandServiceImpl.java
+++ src/main/java/com/youcode/digitalorders/core/service/impl/DemandServiceImpl.java
@@ -3,6 +3,7 @@ package com.youcode.digitalorders.core.service.impl;
 import com.youcode.digitalorders.core.dao.model.entity.Demand;
 import com.youcode.digitalorders.core.dao.repository.DemandRepository;
 import com.youcode.digitalorders.core.service.DemandService;
+import com.youcode.digitalorders.core.service.EquipmentPieceService;
 import com.youcode.digitalorders.core.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,10 +15,11 @@ public class DemandServiceImpl implements DemandService {
 
     private final UserService userService;
     private final DemandRepository demandRepository;
-
+    private final EquipmentPieceService equipmentPieceService;
     @Override
     public Demand create(Demand demand) {
         demand.setUser(userService.findByIdOrThrow(demand.getUser().getId()));
+        demand.setEquipmentPiece(equipmentPieceService.findPiecesByEquipmentId(demand.getEquipmentPiece().getId()).orElseThrow());
         return demandRepository.save(demand);
     }
 }
