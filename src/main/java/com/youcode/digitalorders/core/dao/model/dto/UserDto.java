9b7bbe4d9abaa2e2bf715e6a11faa764ff21592d
abdellah el hilaly <abdellah.elhilaly.96@gmail.com>
2023-11-27 08:54:08 +0100

    delete branch merge for create another empty branch for merge (abdellah ,hmza)

--- src/main/java/com/youcode/digitalorders/core/dao/model/dto/UserDto.java
+++ src/main/java/com/youcode/digitalorders/core/dao/model/dto/UserDto.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
+import org.springframework.format.annotation.NumberFormat;
 
 @Builder
 public class UserDto {
