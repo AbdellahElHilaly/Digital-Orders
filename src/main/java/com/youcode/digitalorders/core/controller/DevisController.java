6a6b3c72ce804660e0f241f44ab3538a06be0c48
marwan-tabib <marwanmaroc297@gmail.com>
2023-11-27 21:37:02 +0100

    marouane | Make src/main/java/com/youcode/digitalorders/core/controller/DevisController.java file, with the getAllEquipments method to return devisService on ResponseEntity

---
+++ src/main/java/com/youcode/digitalorders/core/controller/DevisController.java
@@ -0,0 +1,27 @@
+package com.youcode.digitalorders.core.controller;
+
+import com.youcode.digitalorders.core.dao.model.entity.Devis;
+import com.youcode.digitalorders.core.service.DevisService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static com.youcode.digitalorders.shared.Const.AppEndpoints.DEVIS_ENDPOINT;
+
+@RestController
+@RequestMapping(DEVIS_ENDPOINT)
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+
+public class DevisController {
+
+    private final DevisService devisService;
+
+    @GetMapping()
+    public ResponseEntity<List<Devis>> getAllEquipments() {
+
+        return ResponseEntity.ok(devisService.selectAll());
+    }
+}
