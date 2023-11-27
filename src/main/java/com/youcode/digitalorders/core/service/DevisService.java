50e8ff430cc60e1834f0ed9f4d7a14b4b5a64e01
marwan-tabib <marwanmaroc297@gmail.com>
2023-11-27 21:31:04 +0100

    marouane | Make src/main/java/com/youcode/digitalorders/core/service/DevisService.java file, and add selectAll method with List Devis return type

---
+++ src/main/java/com/youcode/digitalorders/core/service/DevisService.java
@@ -0,0 +1,9 @@
+package com.youcode.digitalorders.core.service;
+
+import com.youcode.digitalorders.core.dao.model.entity.Devis;
+
+import java.util.List;
+
+public interface DevisService {
+    List<Devis> selectAll();
+}
