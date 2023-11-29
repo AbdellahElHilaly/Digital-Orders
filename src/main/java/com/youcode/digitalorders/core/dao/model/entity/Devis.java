c442792e16863f65554336549978cd5cf27e8acb
marwan-tabib <marwanmaroc297@gmail.com>
2023-11-29 09:14:43 +0100

    marouane | Delete Comment ManyToOne Relationship - entity/Devis

--- src/main/java/com/youcode/digitalorders/core/dao/model/entity/Devis.java
+++ src/main/java/com/youcode/digitalorders/core/dao/model/entity/Devis.java
@@ -17,9 +17,9 @@ public class Devis {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "demand_id")
-    private Demand demand;
+    // @ManyToOne
+    // @JoinColumn(name = "demand_id")
+    // private Demand demand;
     private double description;
     private String status;
 
