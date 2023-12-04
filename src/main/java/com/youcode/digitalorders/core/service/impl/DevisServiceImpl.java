deaafbdcc7f827f9ee2f0b1aab58f7af7a3f1e39
marwan-tabib <marwanmaroc297@gmail.com>
2023-12-04 15:12:49 +0100

    marouane | Add createDevi with the logic of get all devis - service/impl/DevisServiceImpl

--- src/main/java/com/youcode/digitalorders/core/service/impl/DevisServiceImpl.java
+++ src/main/java/com/youcode/digitalorders/core/service/impl/DevisServiceImpl.java
@@ -1,20 +1,27 @@
 package com.youcode.digitalorders.core.service.impl;
 
+import com.youcode.digitalorders.core.dao.model.entity.Demand;
 import com.youcode.digitalorders.core.dao.model.entity.Devis;
+import com.youcode.digitalorders.core.dao.repository.DemandRepository;
 import com.youcode.digitalorders.core.dao.repository.DevisRepository;
 import com.youcode.digitalorders.core.service.DevisService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class DevisServiceImpl implements DevisService {
 
     @Autowired
     private DevisRepository devisRepository;
+    @Autowired
+    private DemandRepository demandRepository;
 
     @Override
     public List<Devis> getAllDevis() {
@@ -27,8 +34,26 @@ public class DevisServiceImpl implements DevisService {
     }
 
     @Override
-    public Devis addDevis(Devis devis) {
-        return null;
+    public ResponseEntity<Map<String,Object>> createDevi(Devis devi) {
+        Demand demandToFind = demandRepository.findById(devi.getDemand().getId()).get();
+        Map<String, Object> response = new HashMap<>();
+
+        if (demandToFind != null) {
+            if (demandToFind.getStatus().equals("ACCEPTED")) {
+                response.put("status", "succ");
+                response.put("message", "devi add successfully");
+                response.put("devi", demandToFind);
+                return ResponseEntity.ok(response);
+            } else {
+                response.put("status", "exception");
+                response.put("message", "demand don't have ACCEPTED status!");
+                return ResponseEntity.badRequest().body(response);
+            }
+        }
+        response.put("status", "error");
+        response.put("message", "Please check your demand information!");
+        return ResponseEntity.badRequest().body(response);
+
     }
 
     @Override
