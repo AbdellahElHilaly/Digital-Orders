6d7d1d5d787c68aecd4553be464fd64cecbc8de0
marwan-tabib <marwanmaroc297@gmail.com>
2023-12-04 15:11:42 +0100

    marouane | Add createDevi method with ResponseEntity<Map<String,Object>> type of return - service/DevisService

--- src/main/java/com/youcode/digitalorders/core/service/DevisService.java
+++ src/main/java/com/youcode/digitalorders/core/service/DevisService.java
@@ -2,12 +2,14 @@ package com.youcode.digitalorders.core.service;
 
 import com.youcode.digitalorders.core.dao.model.entity.Contrat;
 import com.youcode.digitalorders.core.dao.model.entity.Devis;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
+import java.util.Map;
 
 public interface DevisService {
     public List<Devis> getAllDevis();
-    public Devis addDevis(Devis devis);
+    public ResponseEntity<Map<String,Object>> createDevi(Devis devi);
     public Devis getDevisById(Long id);
     public Devis findByIdOrThrow(Long id);
 }
