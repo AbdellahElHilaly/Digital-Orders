e845cba80a14d5ee578f1379e66897eb8da2818e
marwan-tabib <marwanmaroc297@gmail.com>
2023-12-06 15:10:21 +0100

    marouane | Change the acceptDevi method with Long id argument - DevisService

--- src/main/java/com/youcode/digitalorders/core/service/DevisService.java
+++ src/main/java/com/youcode/digitalorders/core/service/DevisService.java
@@ -2,14 +2,17 @@ package com.youcode.digitalorders.core.service;
 
 import com.youcode.digitalorders.core.dao.model.entity.Contrat;
 import com.youcode.digitalorders.core.dao.model.entity.Devis;
+import com.youcode.digitalorders.core.dao.model.entity.Equipment;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 public interface DevisService {
     public List<Devis> getAllDevis();
     public ResponseEntity<Map<String,Object>> createDevi(Devis devi);
+    public ResponseEntity<Map<String,Object>> acceptDevi(Long id);
     public Devis getDevisById(Long id);
     public Devis findByIdOrThrow(Long id);
 }
