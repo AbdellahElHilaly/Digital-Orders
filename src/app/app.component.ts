654d6950a9d24e27be226c0e8dff05726c5d55a8
abdellah el hilaly <abdellah.elhilaly.96@gmail.com>
2023-12-07 13:57:50 +0100

    change the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {NavigationComponent} from "./core/view/layout/navigation/navigation.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatSlideToggleModule, NavigationComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
